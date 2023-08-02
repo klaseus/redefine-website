33fdc94d4ee5d1f38e9c7ccdfe3b38b87d388006
Klazeus <klas.grahns@gmail.com>
2023-08-02 12:39:39 +0200

    removed storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,6 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
 
-const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: 'eu', // optional,  or 'eu' (default)
-      },
-    })
-  ],
+ 
 });
